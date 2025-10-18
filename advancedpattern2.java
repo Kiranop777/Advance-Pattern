ca20d2c9a81eb36c3bbe5c144d8c93b25f464790
Kiranop777 <kirandg18@gmail.com>
2025-10-18 10:08:39 +0530

    Create advancedpattern2.java

---
+++ advancedpattern2.java
@@ -0,0 +1,23 @@
+public class advancedpattern2{
+    public static void main(String[] args) {
+        int n = 4; // Number of rows
+        int m = 4; // Number of columns
+        printPattern(n, m);
+    }
+
+    public static void printPattern(int n, int m) {
+        // Loop through each row
+        for (int i = 1; i <= n; i++) {
+            // Loop through each column
+            for (int j = 1; j <= m; j++) {
+                // Check the conditions to print '*'
+                if ( i == n || j == m || (i + j) == (m + 1)|| (i+j)> (m+1) ) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
