48569c2c0e246ae2af09e49ce010784f45485e87
Kiranop777 <kirandg18@gmail.com>
2025-10-21 12:04:59 +0530

    Create advancedpattern3.java

---
+++ advancedpattern3.java
@@ -0,0 +1,18 @@
+public class advancedpattern3{
+    public static void pat(int n){
+        for(int i = 1; i <= n; i++){
+          int ch = 1;
+            for(int j = 1; j <=(n-i+1); j++){
+                System.out.print(ch);
+                ch++;
+           
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        int n = 5;
+        pat(n);
+    }
+    
+}
